6e982042c6044258aef97a6cb197fe94f05c6afd
Michael Bradvica <mjbradvica@gmail.com>
2024-08-25 17:01:10 -0400

    styling and styling linting

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./app/App.vue";
 import router from "./router";
+import "./styles/index.scss";
 
 const app = createApp(App);
 
