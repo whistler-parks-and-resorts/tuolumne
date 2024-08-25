921e2b53a12b9c2be791d9f3017d8db9a0ab96e3
Michael B <mjbradvica@gmail.com>
2024-08-25 11:51:27 -0400

    prettier run

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import tseslint from "typescript-eslint";
+import eslint from "@eslint/js";
+import angular from "angular-eslint";
+import prettier from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettier,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+      prettier,
+    ],
+    rules: {},
+  },
+);
